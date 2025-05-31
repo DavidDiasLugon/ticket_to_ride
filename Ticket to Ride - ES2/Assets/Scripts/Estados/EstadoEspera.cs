bd1718eb425d323819a12026d29529bbe790c2ef
Fucking Magician <jpedrotx2801@gmail.com>
2025-05-30 22:27:28 -0300

    Mais adições na lógica do jogo
    Foi implementada a lógica de cartas abertas.

--- Ticket to Ride - ES2/Assets/Scripts/Estados/EstadoEspera.cs
+++ Ticket to Ride - ES2/Assets/Scripts/Estados/EstadoEspera.cs
@@ -7,6 +7,7 @@ public class EstadoEspera : EstadoJogo
     private Button botaoCompraBilhete;
     public override void IniciarEstado(Controle controle)
     {
+        controle.JogadorAtual.UpdateNumeroCartasDict();
         _GameManager.Instance.maoCartas.AtualizaExibicao(controle.JogadorAtual.CartaNmr);
         Debug.Log("Turno de: " + controle.JogadorAtual.Nome);
         botaoCompraCarta = GameObject.Find("BotaoCarta")?.GetComponent<Button>();
@@ -21,6 +22,7 @@ public class EstadoEspera : EstadoJogo
         {
             Debug.Log("Botão Compra Carta Clicado");
             botaoCompraCarta.onClick.RemoveAllListeners();
+            botaoCompraBilhete.onClick.RemoveAllListeners();
             controle.TrocaEstado(EstadoCompraCarta1.CreateInstance<EstadoCompraCarta1>());
         });
 
@@ -28,11 +30,32 @@ public class EstadoEspera : EstadoJogo
         {
             Debug.Log("Botão Compra Bilhete Clicado");
             botaoCompraBilhete.onClick.RemoveAllListeners();
+            botaoCompraCarta.onClick.RemoveAllListeners();
             controle.TrocaEstado(EstadoCompraBilhete.CreateInstance<EstadoCompraBilhete>());
         });
     }
 
     public override void RunEstado(Controle controle)
     {
+    }
+
+    public override void ProcessarSelecao(Controle controle, int indice, Carta cartaSelecionada)
+    {
+        Debug.Log("Processando seleção de carta");
+        controle.JogadorAtual.MaoCartas.Add(cartaSelecionada);
+        controle.CartasAbertas.RemoveAt(indice);
+        Carta c = controle.DeckCartas.CompraCarta();
+        controle.CartasAbertas.Add(c);
+        controle.VerificaLocomotivas();
+        _GameManager.Instance.cartasAbertas.AtualizaExibicaoCartasAbertas(controle.CartasAbertas);
+        botaoCompraCarta.onClick.RemoveAllListeners();
+        if (cartaSelecionada.isLocomotive)
+        {
+            controle.TrocaEstado(EstadoFimTurno.CreateInstance<EstadoFimTurno>());
+        }
+        else
+        {
+            controle.TrocaEstado(EstadoEspera2.CreateInstance<EstadoEspera2>());
+        }
     }
 }
--- Ticket to Ride - ES2/Assets/Scripts/Estados/EstadoJogo.cs
+++ Ticket to Ride - ES2/Assets/Scripts/Estados/EstadoJogo.cs
@@ -5,4 +5,5 @@ public abstract class EstadoJogo : ScriptableObject
     public abstract void IniciarEstado(Controle controle);
     public abstract void RunEstado(Controle controle);
     //Cada fim de Estado tem uma chamada a função do Troca de Estado
+    public abstract void ProcessarSelecao(Controle controle, int indice, Carta cartaSelecionada);
 }
